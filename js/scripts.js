37ebcba88246a764d28e60ca0042763708706729
James Cho <hwisoocho@gmail.com>
2018-10-26 09:36:48 -0700

    add boop, beep logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 // Back-end Logic
+var msg = "";
+
 function beepBoop(){
 
   var num = $("input#number").val();
-  var msg = "";
+  var contains1 = num.includes("1");
+  var contains0 = num.includes("0");
+  
   console.log("number was " + num);
 if (num % 3 === 0) {
   msg = "I'm sorry. I'm afraid I can't do that."; 
-} 
+} else if (contains1){
+  msg = "Boop!";
+} else if (contains0) {
+  msg = "Beep!";
+} else {
+  msg = num;
+}
 
 
-$("#output").append("<li>" + msg + "</li>");
+
 }
 
 
@@ -21,6 +31,6 @@ $(document).ready(function(){
   $("form#form").submit(function(){
     event.preventDefault();
     beepBoop();
-    
+    $("#output").append("<li>" + msg + "</li>");
   });
 });
