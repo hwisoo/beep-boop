373184f89629907226e6e539987fd0cbd5147d9a
James Cho <hwisoocho@gmail.com>
2018-10-26 22:20:35 -0700

    clean up code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
 // Back-end Logic
-var msg = "";
 
 function beepBoop(){
   
@@ -31,39 +30,24 @@ function beepBoop(){
 
   console.log(strNumbers);
   
-
-
-
-
+  strNumbers.forEach(function(item){
   
-  // console.log(num);
-  // var includes1 = num.includes("1");
-  // var includes0 = num.includes("0");
-  
-  var beep = false;
-  var boop = false;
-  var error = false;
-  num = Array.from(num.toString()).map(Number);
-
-  
-  // Play audio based on condition
-  if(error){
-  $("#error")[0].play();
-  } else if (boop) {
-  $("#boop")[0].play();
-  } else if (beep) {
-  $("#beep")[0].play();
-  };   
-};
+    $("#output").append("<li>" + item + "</li>");
+  })
+}
+ 
 
 // Front-end Logic
 $(document).ready(function(){
   $("form#form").submit(function(){
     event.preventDefault();  
+    $("#output").fadeOut();
+    $("li").each(function(){
+      $(this).remove()
+    });
     
     beepBoop(); 
     
-    $("#output").append("<li>" + item + "</li>");
     $("li:contains('eep')").css('color','blue').css('list-style-type','circle');
     $("li:contains('oop')").css('color','green').css('list-style-type', 'square');
     $("li:contains('sorry')").css('color','red');
